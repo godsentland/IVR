2d726a8f9445dbff046ffcc5bc1964a7ba350482
godsentland <sanyock2003@yandex.ru>
2020-09-29 22:39:42 +0300

    Еще одна версия игрушки
    Добавлены меню паузы, настройки звука (соответственно добавлена музыка)

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject pauseButton;
+    public GameObject gameUIpanel;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        pauseButton.SetActive(true);
+        gameUIpanel.SetActive(true);
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        pauseButton.SetActive(false);
+        gameUIpanel.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/VolumeValue.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeValue : MonoBehaviour
+{
+    private AudioSource audioSrc;
+    private float musicVolume = 1f;
+
+    public Slider slider;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        //slider = GetComponent<Slider>();
+        if (PlayerPrefs.HasKey("SliderValue"))
+        {
+            slider.value = PlayerPrefs.GetFloat("SliderValue");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+        float SlVal = slider.value;
+        PlayerPrefs.SetFloat("SliderValue", SlVal);
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
