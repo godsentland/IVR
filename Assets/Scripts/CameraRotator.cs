eac403af8cafb7cf71cd6eae5f391329bc75cea9
godsentland <sanyock2003@yandex.ru>
2020-10-01 22:45:17 +0300

    Версия к показу кода 2

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -20,7 +20,7 @@ public class CameraRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xRot += touchField.TouchDistance.y / 20;
+        xRot += touchField.TouchDistance.y / 10;
         xRot = xRot % 360;
         xRot = Mathf.Clamp(xRot, minX, maxX); //ограничиваем xRot
         Quaternion yQuaternion = Quaternion.AngleAxis(xRot, Vector3.left);
